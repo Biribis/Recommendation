c6c28378b5bbbf0e2c3c0b2d422e2b4555c9e2a9
GabrielMousinho <126831870+GabrielMousinho@users.noreply.github.com>
2024-03-22 16:37:35 -0300

    Add files via upload

---
+++ Back/DAO.py
@@ -0,0 +1,64 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+
+class DAO:
+
+   def __init__(self, tab):
+       # Ligação com o esquema de banco de dados
+       engine = create_engine("mysql+mysqlconnector://root:uniceub@localhost/bdbs?charset=utf8mb4") # arrumar com um endereço novo
+
+       # Mapeamento Objeto Relacional com o SQLAlchemy
+       db = automap_base()
+       db.prepare(autoload_with=engine)
+       self.tb_usuario = db.classes.tb_usuario
+
+       self.id = "id_" + tab[3:len(tab)]
+
+       # Trabalho com sessões da base Objeto-Relacional
+       session_factory = sessionmaker(bind=engine)
+       self.ses = session_factory()
+       # -------------------------------------------------------------------------------------------------
+
+   def create(self, obj):
+       self.ses.add(obj)
+       self.ses.commit()
+
+   def readAll(self):
+       lista = self.ses.query(self.tabela).all()
+       return lista
+
+   def readById(self, id):
+       exp = "self.tabela." + self.id + "==id"
+       obj = self.ses.query(self.tabela).filter(eval(exp)).first()
+       return obj
+
+   def readBy(self, campo, oper, valor):
+
+       if oper == "==":
+           exp = "self.tabela." + campo + "==valor"
+       elif oper == "ilike":
+           exp = "self.tabela." + campo + ".ilike('%' + valor + '%')"
+       else:
+           exp = "self.tabela." + campo + oper + "valor"
+
+       lista = self.ses.query(self.tabela).filter(eval(exp)).all()
+       return lista
+
+   def update(self):
+       self.ses.commit()
+
+   def delete(self, obj):
+       self.ses.delete(obj)
+       self.ses.commit()
+
+   def getSes(self):
+       return self.ses
+
+   def __del__(self):
+       self.ses.close()
+
+
+if __name__ == '__main__':
+   main()
+
