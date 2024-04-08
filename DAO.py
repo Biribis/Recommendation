71c8d537a1dbf36b0efe30ff57b9e5e40733f885
Felipe Trindade <marshadollucha@gmail.com>
2024-04-07 21:12:31 -0300

    Mudanca na estrutar para que o flask identifique os html e funcionamento da pagina inicial

---
+++ DAO.py
@@ -0,0 +1,69 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+
+class DAO:
+
+   name = "Rec"
+   password = "quemleumecomeu"
+   host = "localhost"
+   database = "Recommendation"
+
+   def __init__(self, tab):
+       # Ligação com o esquema de banco de dados
+       engine = create_engine("mysql+mysqlconnector://" + self.name + ":" + self.password + "@" + self.host + "/" + self.database + "?charset=utf8mb4") # arrumar com um endereço novo
+
+       # Mapeamento Objeto Relacional com o SQLAlchemy
+       db = automap_base()
+       db.prepare(autoload_with=engine)
+       self.tb_usuario = db.classes.tb_usuario
+       self.tb_jogos = db.classes.tb_jogos
+       self.tb_usuario_jogos = db.classes.tb_usuario_jogos
+
+       self.id = "id_" + tab[3:len(tab)]
+
+       # Trabalho com sessões da base Objeto-Relacional
+       session_factory = sessionmaker(bind=engine)
+       self.ses = session_factory()
+       # -------------------------------------------------------------------------------------------------
+
+   def create(self, obj):
+       self.ses.add(obj)
+       self.ses.commit()
+
+   def readAll(self):
+       lista = self.ses.query(self.tabela).all()
+       return lista
+
+   def readById(self, id):
+       exp = "self.tabela." + self.id + "==id"
+       obj = self.ses.query(self.tabela).filter(eval(exp)).first()
+       return obj
+
+   def readBy(self, campo, oper, valor):
+
+       if oper == "==":
+           exp = "self.tabela." + campo + "==valor"
+       elif oper == "ilike":
+           exp = "self.tabela." + campo + ".ilike('%' + valor + '%')"
+       else:
+           exp = "self.tabela." + campo + oper + "valor"
+
+       lista = self.ses.query(self.tabela).filter(eval(exp)).all()
+       return lista
+
+   def update(self):
+       self.ses.commit()
+
+   def delete(self, obj):
+       self.ses.delete(obj)
+       self.ses.commit()
+
+   def getSes(self):
+       return self.ses
+
+   def __del__(self):
+       self.ses.close()
+
+
+
